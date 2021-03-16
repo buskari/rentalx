0b88b512f221aae0cc4c66a9a7013eeea8abc4c0
Andris Buscariolli <andrisbuscariolli88@gmail.com>
2021-03-15 23:35:57 -0300

    created category class and repository

--- routes/categories.routes.ts
+++ routes/categories.routes.ts
@@ -1,18 +1,27 @@
 import { Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description
-  });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'This category already exists' });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+})
+
+categoriesRoutes.get('/', (request, response) => {
+  const allCategories = categoriesRepository.list();
+
+  return response.json(allCategories);
 })
 
 export { categoriesRoutes };
